4103a91a61f946c860a8f60e3123d6e2a66b99fa
Bradley Bergerhouse <bergerhouseb@gmail.com>
2017-08-12 18:44:48 -0500

    Comments and some cleanup

--- WorkingasDesigned/app/src/main/java/com/pch2017/bergebf/workingasdesigned/MapActivity.java
+++ WorkingasDesigned/app/src/main/java/com/pch2017/bergebf/workingasdesigned/MapActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,17 +17,9 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.data.Feature;
-import com.google.maps.android.data.Geometry;
-import com.google.maps.android.data.MultiGeometry;
-import com.google.maps.android.data.geojson.GeoJsonFeature;
 import com.google.maps.android.data.geojson.GeoJsonLayer;
-import com.google.maps.android.data.geojson.GeoJsonMultiPolygon;
-import com.google.maps.android.data.geojson.GeoJsonPolygon;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -63,10 +53,10 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             /** Called when a drawer has settled in a completely closed state. */
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
-//                getActionBar().setTitle(mTitle);
-//                getActionBar().setTitle("Test");
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                invalidateOptionsMenu();
 
+                //iterate through each layer and show/hide as necessary
+                //Doign this when the Drawer closes, because it is easy
                 for(int i = 0; i < 5; i++) {
                     if(!(layers[i] == null)) {
                         if (layerToggles[i]) {
@@ -81,24 +71,23 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             /** Called when a drawer has settled in a completely open state. */
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-//                getActionBar().setTitle("Test");
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                invalidateOptionsMenu();
             }
         };
 
         // Set the drawer toggle as the DrawerListener
         mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-        // Set the adapter for the list view
-//        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mDrawerTitles));
         // Set the list's click listener
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
+        //Initially hide all layers
         for(int i = 0; i < 5; i++) {
             layerToggles[i] = false;
         }
     }
 
+    //Event listener for when Drawer items are selected
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -111,42 +100,40 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     }
 
 
-    /**mDrawerTitle
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        //Move Camera to Peoria
+        //Move Camera to Peoria and set the zoom
         LatLng peoria = new LatLng(40.692751, -89.594682);
-//        mMap.addMarker(new MarkerOptions().position(peoria).title("Peoria"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(peoria, 11));
         try {
-            layers[0] = new GeoJsonLayer(mMap, R.raw.arterialrecon, getApplicationContext());
+            //Import each type of information into its own layer
+            layers[0] = new GeoJsonLayer(mMap, R.raw.arterialrecon2, getApplicationContext());
             layers[1] = new GeoJsonLayer(mMap, R.raw.pavement2017, getApplicationContext());
             layers[2] = new GeoJsonLayer(mMap, R.raw.sidewalkconstruction, getApplicationContext());
             layers[3] = new GeoJsonLayer(mMap, R.raw.residential, getApplicationContext());
             layers[4] = new GeoJsonLayer(mMap, R.raw.pendingpermits, getApplicationContext());
 
+            //For simplicity, only the Arterial Construction layer will provide details.
+            //TODO: make this work for the other data types.  Data types should have a more
+            //      generalized structure.
             layers[0].setOnFeatureClickListener(new GeoJsonLayer.OnFeatureClickListener() {
                 @Override
                 public void onFeatureClick(Feature feature) {
                     TextView mtext;
                     Button mButton;
+
+                    //Create a dialog box to rpesent teh user with additional information
                     final Dialog dialog = new Dialog(MapActivity.this);
                     dialog.setContentView(R.layout.details_dialog);
                     dialog.setTitle("Project Details");
+
+                    //Fill in the text fields with the information from the GeoJSON data
                     mtext = (TextView) dialog.findViewById(R.id.textView5);
                     mtext.setText(feature.getProperty("ProjStreet"));
                     mtext = (TextView) dialog.findViewById(R.id.textView7);
-                    mtext.setText(feature.getProperty("EstState"));
+                    mtext.setText(feature.getProperty("EstStart"));
                     mtext = (TextView) dialog.findViewById(R.id.textView9);
                     mtext.setText(feature.getProperty("EstEnd"));
                     mtext = (TextView) dialog.findViewById(R.id.textView11);
@@ -154,10 +141,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                     mtext = (TextView) dialog.findViewById(R.id.textView13);
                     mtext.setText("Chuck's Construction");
 
+                    //Add a click handler to allow teh user to close the dialog
                     mButton = (Button) dialog.findViewById(R.id.button4);
-
-                    Log.i("GeoJsonClick", "Feature clicked: " + feature.getProperty("ProjStreet"));
-
                     mButton.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
@@ -170,9 +155,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             });
 
         } catch (Exception e) {
-
+            //This SHOULD actually do something.  Meh.
         }
-
-
     }
 }
